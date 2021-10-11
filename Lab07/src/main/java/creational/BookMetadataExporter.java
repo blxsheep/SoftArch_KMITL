e9664baeb28b26f2e1889a8f83ef3264984aaa98
blxsheep <por2543rk@gmail.com>
2021-10-12 00:04:28 +0700

    Finished

--- Lab07/src/main/java/creational/BookMetadataExporter.java
+++ Lab07/src/main/java/creational/BookMetadataExporter.java
@@ -7,21 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BookMetadataExporter extends BookCollection {
+    BookMetadataFormatter Formatter = null;
     public abstract BookMetadataFormatter createFormatter() throws ParserConfigurationException, IOException;
     public void export(PrintStream stream) throws ParserConfigurationException {
-        // Please implement this method. You may create additional methods as you see fit.
-        BookMetadataFormatter Formatter = null;
-        try {
-            Formatter = createFormatter();
-        } catch (IOException e) {
-            e.printStackTrace();
+        stream.println(this.Formatter.getMetadataString());
+    }
+    public void add(Book b){
+        if(this.Formatter == null ){
+            try {
+                Formatter = createFormatter();
+            } catch (IOException | ParserConfigurationException e) {
+                e.printStackTrace();
+            }
         }
-        List<Book> book_list = new ArrayList<Book>(this.books);
-
-        for(int k = 0 ; k < book_list.size(); k+=1){
-            Formatter.append(book_list.get(k));
-        }
-        stream.println(Formatter.getMetadataString());
+        Formatter.append(b);
 
     }
 
